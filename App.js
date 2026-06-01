d33e85d6f3d24031604e5adae6df8f2ee70b9ded
johnsravya20 <johnsravya20@gmail.com>
2026-06-02 00:43:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+* { box-sizing: border-box; margin: 0; padding: 0; }
+body {
+  background: #0d0d1a;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  overflow-x: hidden;
+}
